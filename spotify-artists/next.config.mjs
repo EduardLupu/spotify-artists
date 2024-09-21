287437c9daf654c60a1e1055372db6810899b9f0
EduardLupu <lupu.edward@yahoo.com>
2024-09-21 15:37:09 +0300

    Fixed basePath

--- spotify-artists/next.config.mjs
+++ spotify-artists/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: process.env.NODE_ENV === 'production' ? 'spotify-artists/spotify-artists' : '',
+    basePath: process.env.NODE_ENV === 'production' ? '/spotify-artists/spotify-artists' : '',
     assetPrefix: process.env.NODE_ENV === 'production' ? 'https://spotify-artists.eduardlupu.io/' : '',
     images: process.env.NODE_ENV === 'production' ? {
         loader:'akamai',
