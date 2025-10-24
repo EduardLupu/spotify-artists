2189c53cc7e2fe04c7933f7ec074f917a35d1bea
EduardLupu <lupu.edward@yahoo.com>
2025-10-24 17:48:24 +0300

    Added CNAME.

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: process.env.NODE_ENV === 'production' ? '/spotify-artists' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://eduardlupu.github.io/spotify-artists/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/music.eduardlupu.com' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://music.eduardlupu.com/' : '',
   images: process.env.NODE_ENV === 'production' ? {
     loader:'akamai',
-    path: 'https://eduardlupu.github.io/spotify-artists/'
+    path: 'https://music.eduardlupu.com/',
+    unoptimized: true
   } : {},
   output: process.env.NODE_ENV === 'production' ? 'export' : 'standalone'
 };
